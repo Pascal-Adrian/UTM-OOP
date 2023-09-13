961c4aee04a19970d174ffd0889c77e98c27658e
Pascal Adrian <aa4033339@icloud.com>
2023-09-13 09:48:17 +0300

    first commit

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    String name;
+    String surname;
+    boolean willPass;
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setSurname(String s) {
+        surname = s;
+    }
+    public void getName() {
+        System.out.println(name);
+    }
+}
